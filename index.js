68f096970e8ff9f0e6c8b9810b9e520f17d80a57
Nikhil Athawale <nathawale@delaplex.com>
2024-05-07 18:40:48 +0530

    Reorganize folder structure

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const serverless = require("serverless-http");
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./model/product.model.js');
-const productRoute = require("./routes/product.route.js")
+const Product = require('./src/app/models/product.model.js');
+const productRoute = require("./src/routes/product.route.js");
 const app = express();
+require('./src/config/db_connect.js')
 
 //middleware
 app.use(express.json());
@@ -12,16 +14,18 @@ app.use(express.urlencoded({extended:false}))
 app.use("/api/products",productRoute);
 
 
-mongoose.connect("mongodb://localhost/newdb")
-.then((db) => {
-   console.log('DB Connected');
-})
-.catch((err) => {
-    console.log('Error Detected')
-})
+// mongoose.connect("mongodb://localhost/newdb")
+// .then((db) => {
+//    console.log('DB Connected');
+// })
+// .catch((err) => {
+//     console.log('Error Detected')
+// })
 
 app.get('/',(req,res) => {
         res.send('Hello LOvely World! Hello Hii')
 })
 
-app.listen(3000)
+//app.listen(3000)
+
+module.exports.main = serverless(app);
